6fa29705af48332fc119dbb01b110795e27a18b3
github username <github email address>
2019-06-23 01:44:40 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,12 @@
 class Application
 
+  # Return a response which consists of the status code, any headers, and the body
+
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    # add some text "Hello, World" to the body
+    resp.write "Hello, my name is Rostam"
+    # complete the response
     resp.finish
   end
-
 end
-
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+# to actually set up an HTTP web server that will receive the HTTP request, send it through the  #call method, and then serve the response to the browser.
+
 require_relative "./application.rb"
 
 run Application.new
