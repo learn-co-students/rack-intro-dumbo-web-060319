7aaaffef7e75154f1ab641afa452467acbaaf058
Francis Mendez <fmendez884@gmail.com>
2019-06-23 16:40:53 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World, my name is what my name is who"
+    resp.write "Hello, my name is what my name is who"
     resp.finish
   end
 
